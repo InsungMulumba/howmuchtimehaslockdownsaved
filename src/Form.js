4ad4f23c72ec06d41cec314f3c7d152fb875a700
insung-mulumba <insung.mulumba@just-eat.com>
2020-04-22 22:52:05 +0100

    Initial styling

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Form.scss';
+
+function Form() {
+    return (
+      <div className="container">
+         Hello
+      </div>
+    );
+  }
+
+export default Form;
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/NavBar.scss';
+
+const title = <div> How much time did Lockdown save?</div> 
+
+function NavBar() {
+  return (
+    <nav className="navBar">
+       {title}
+    </nav>
+  );
+}
+
+export default NavBar;
